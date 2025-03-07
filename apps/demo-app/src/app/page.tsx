cba05796d0be72f2ae44d5f75702cc69178f1ab2
Andy Swinburne <andy@swinburne.dev>
2025-03-07 11:23:43 +0000

    Add button to demo app

--- apps/demo-app/src/app/page.tsx
+++ apps/demo-app/src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from '@nx-demo-multi-app/Button';
+
 import styles from './page.module.scss';
 
 export default function Index() {
@@ -16,6 +18,8 @@ export default function Index() {
               Welcome nx-demo-multi-app 👋
             </h1>
           </div>
+
+          <Button />
 
           <div id="hero" className="rounded">
             <div className="text-container">
